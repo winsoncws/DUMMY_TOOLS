711528e1143331a995547642c0df2a3a8e947984
Winson Chan <winsoncws84@gmail.com>
2022-09-16 10:43:50 +0800

    main.py

---
+++ dummycode/main.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import numpy
+
+class Main:
+    """
+    this is Main Class
+    """
+    
+    def __init__(self):
+        """
+        initialize the class
+        """
+        pass
+    
+    def forward(self, x):
+        """
+        forward process
+
+        Parameters
+        ----------
+        x : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+        pass
